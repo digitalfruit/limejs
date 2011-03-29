d9dbbfeb4c1d79612671602d62f494e67b618343
Tõnis Tiigi <tonis@digitalfruit.info>
2011-03-29 17:38:40 +0300

    Faster multiply from glMatrix

--- lime/src/webgl/webgl.js
+++ lime/src/webgl/webgl.js
@@ -411,36 +411,39 @@ lime.webgl.M.prototype.inverse = function() {
 
 
 lime.webgl.M.prototype.multiply = function(matrix){
-    //console.log(matrix);
     if(goog.isNumber(matrix)){
         return this.scale(matrix);
     }
-    var returnVector = goog.isDef(matrix.msize) ? false : true;
-    var M = matrix.elements || matrix;
-    if(this.elements[0].length != matrix.length){
-        if(goog.DEBUG){
-            throw('Impossible multiply '+this.elements[0].length+' x '+ matrix.length);
-        }
-        return null;
-    }//console.log(M);
-    var ni = this.msize, ki = ni, i, nj, kj = returnVector ? matrix.size : matrix.msize, j;
-    var cols = this.msize, elements = [], sum, nc, c;
-    do { i = ki - ni;
-      elements[i] = [];
-      nj = kj;
-      do { j = kj - nj;
-        sum = 0;
-        nc = cols;
-        do { c = cols - nc;
-          sum += this.elements[c*this.msize+i] * M[kj*j+c];
-         // console.log('sum '+i+' '+(c*this.msize+i)+' '+this.elements[c*this.msize+i]+' '+M[kj*j+c]);
-        } while (--nc);
-        elements[i][j] = sum;//console.log('set '+sum)
-      } while (--nj);
-    } while (--ni);
-    if(returnVector) return new lime.webgl.V(elements[0].length,this.type,elements[0]);
+    var mat = this.elements,mat2=matrix.elements;
     
-    return this.set(elements);
+	// Cache the matrix values (makes for huge speed increases!)
+	var a00 = mat[0], a01 = mat[1], a02 = mat[2], a03 = mat[3];
+	var a10 = mat[4], a11 = mat[5], a12 = mat[6], a13 = mat[7];
+	var a20 = mat[8], a21 = mat[9], a22 = mat[10], a23 = mat[11];
+	var a30 = mat[12], a31 = mat[13], a32 = mat[14], a33 = mat[15];
+	
+	var b00 = mat2[0], b01 = mat2[1], b02 = mat2[2], b03 = mat2[3];
+	var b10 = mat2[4], b11 = mat2[5], b12 = mat2[6], b13 = mat2[7];
+	var b20 = mat2[8], b21 = mat2[9], b22 = mat2[10], b23 = mat2[11];
+	var b30 = mat2[12], b31 = mat2[13], b32 = mat2[14], b33 = mat2[15];
+	
+	mat[0] = b00*a00 + b01*a10 + b02*a20 + b03*a30;
+	mat[1] = b00*a01 + b01*a11 + b02*a21 + b03*a31;
+	mat[2] = b00*a02 + b01*a12 + b02*a22 + b03*a32;
+	mat[3] = b00*a03 + b01*a13 + b02*a23 + b03*a33;
+	mat[4] = b10*a00 + b11*a10 + b12*a20 + b13*a30;
+	mat[5] = b10*a01 + b11*a11 + b12*a21 + b13*a31;
+	mat[6] = b10*a02 + b11*a12 + b12*a22 + b13*a32;
+	mat[7] = b10*a03 + b11*a13 + b12*a23 + b13*a33;
+	mat[8] = b20*a00 + b21*a10 + b22*a20 + b23*a30;
+	mat[9] = b20*a01 + b21*a11 + b22*a21 + b23*a31;
+	mat[10] = b20*a02 + b21*a12 + b22*a22 + b23*a32;
+	mat[11] = b20*a03 + b21*a13 + b22*a23 + b23*a33;
+	mat[12] = b30*a00 + b31*a10 + b32*a20 + b33*a30;
+	mat[13] = b30*a01 + b31*a11 + b32*a21 + b33*a31;
+	mat[14] = b30*a02 + b31*a12 + b32*a22 + b33*a32;
+	mat[15] = b30*a03 + b31*a13 + b32*a23 + b33*a33;
+	return this;
 };
 
 lime.webgl.M.prototype.transpose = function(){
