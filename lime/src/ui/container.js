e67bc0af7f3f8db5faac46278abc1a0befa0f6b0
Tõnis Tiigi <tonis@digitalfruit.info>
2011-04-05 16:54:39 +0300

    Vertical orientation support

--- lime/src/ui/container.js
+++ lime/src/ui/container.js
@@ -12,6 +12,8 @@ lime.ui.Container = function(){
 
    this.toAdd_ = [];
    this.toRemove_ = [];
+   
+   this.setDirection(lime.ui.Container.Direction.HORIZONTAL);
 
 };
 goog.inherits(lime.ui.Container, lime.Layer);
@@ -32,17 +34,47 @@ lime.ui.Container.prototype.removeChild = function(child,opt_animate){
     
 //}
 
+/**
+ * Directions of the scroller.
+ * @enum number
+ */
+lime.ui.Container.Direction = {
+  HORIZONTAL: 0,
+  VERTICAL: 1
+};
+
+/**
+ * Returns the direction of the scroller (horizontal/vertical)
+ * @return {lime.ui.Scroller.Direction} Scroll direction.
+ */
+lime.ui.Container.prototype.getDirection = function() {
+    return this.direction_;
+};
+
+/**
+ * Set the direction of the scroller (horizontal/vertical)
+ * @param {lime.ui.Scroller.Direction} direction Direction.
+ * @return {lime.ui.Scroller} object itself.
+ */
+lime.ui.Container.prototype.setDirection = function(direction) {
+    this.direction_ = direction;
+    return this;
+};
+
+
 lime.ui.Container.prototype.update = function(opt_pass){
     if(!opt_pass){
         var l = this.children_.length;
-        var pos = new goog.math.Coordinate(this.GAP,0);
+        var pos = new goog.math.Coordinate(0,0);
         for(var i=0;i<l;i++){
             var child = this.children_[i];
             var p2 = child.getPosition();;
             if(!goog.math.Coordinate.equals(pos,p2)){
                 child.setPosition(pos.clone());
             }
+            if(this.getDirection()==lime.ui.Container.Direction.HORIZONTAL)
             pos.x+=child.getSize().width+this.GAP;
+            else pos.y+=child.getSize().height+this.GAP;
         }
     }
     
