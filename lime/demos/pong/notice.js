06b91999e591b223915f871138c39f4e8d9e0b3d
Russ <russplaysguitar@yahoo.com>
2011-09-14 23:37:47 -0700

    Fixed missing score label and too long text

--- lime/demos/pong/notice.js
+++ lime/demos/pong/notice.js
@@ -11,8 +11,11 @@ pong.Notice = function() {
 
     this.setSize(150, 100).setFill(back).setAnchorPoint(.5, 0);
 
-    this.title = new lime.Label().setText('Click anywhere to begin!').setPosition(0, 20);
+    this.title = new lime.Label().setText('Click to begin!').setPosition(0, 20);
     this.appendChild(this.title);
+    
+    this.score = new lime.Label().setText('').setPosition(0, 60).setFontSize(34);   
+    this.appendChild(this.score);
 };
 goog.inherits(pong.Notice, lime.RoundedRect);
 
