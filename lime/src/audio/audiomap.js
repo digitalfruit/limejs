166cc9919188d43a1f4bedbace5282480b9dd467
Tõnis Tiigi <tonistiigi@gmail.com>
2012-04-19 18:36:12 +0300

    Fix tabs

--- lime/src/audio/audiomap.js
+++ lime/src/audio/audiomap.js
@@ -11,21 +11,21 @@ goog.require('jukebox.Player');
  * @param {object} config Configuration object.
  */
 lime.audio.AudioMap = function(config) {
-	if (config.data && goog.isFunction(config.data)) {
-		config = config.data();
-	}
-	this.config = config;
-	
-	if (lime.userAgent.IOS || lime.userAgent.WINPHONE) {
-		goog.events.listenOnce(window, lime.userAgent.SUPPORTS_TOUCH ? 'touchstart' : 'mousedown', this._initPlayer, true, this);
-	}
-	else {
-		this._initPlayer();
-	}
+    if (config.data && goog.isFunction(config.data)) {
+        config = config.data();
+    }
+    this.config = config;
+    
+    if (lime.userAgent.IOS || lime.userAgent.WINPHONE) {
+        goog.events.listenOnce(window, lime.userAgent.SUPPORTS_TOUCH ? 'touchstart' : 'mousedown', this._initPlayer, true, this);
+    }
+    else {
+        this._initPlayer();
+    }
 };
 
 lime.audio.AudioMap.prototype._initPlayer = function() {
-	this.player = new jukebox.Player(this.config);
+    this.player = new jukebox.Player(this.config);
 }
 
 /**
@@ -33,18 +33,18 @@ lime.audio.AudioMap.prototype._initPlayer = function() {
  * @param {string} sprite Sprite name to play. 
  */
 lime.audio.AudioMap.prototype.play = function(sprite) {
-	if (this.player && this.config.spritemap[sprite]) {
-		this.player.play(sprite);
-	}
+    if (this.player && this.config.spritemap[sprite]) {
+        this.player.play(sprite);
+    }
 };
 
 /**
  * Stop playing the audio
  */
 lime.audio.AudioMap.prototype.stop = function() {
-	if (this.player) {
-		this.player.pause();
-	}
+    if (this.player) {
+        this.player.pause();
+    }
 };
 
 
