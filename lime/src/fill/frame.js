ee435978503376286b0957cc1252a6a28d1e4c97
Tõnis Tiigi <tonistiigi@gmail.com>
2013-05-14 10:45:24 +0300

    Fix frame fill for canvas only + formatting

--- lime/src/fill/frame.js
+++ lime/src/fill/frame.js
@@ -9,6 +9,7 @@ goog.require('goog.dom.classes');
 goog.require('goog.dom');
 goog.require('goog.math.Vec2');
 goog.require('goog.math.Size');
+goog.require('lime.dom');
 
 /**
  * Image fill.
@@ -22,19 +23,19 @@ goog.require('goog.math.Size');
  */
 lime.fill.Frame = function(img, rect, opt_offset, opt_size, opt_rotated) {
     lime.fill.Image.call(this,img);
-    
+
     if(goog.isNumber(rect)){
         rect = new goog.math.Rect(arguments[1],arguments[2],arguments[3],arguments[4]);
         opt_offset = new goog.math.Vec2(0,0);
         opt_size = new goog.math.Size(rect.width,rect.height);
         opt_rotated = false;
     }
-    
+
     this.rect_ = rect;
     this.coffset_ = opt_offset;
     this.csize_ = opt_size;
     this.rotated_ = opt_rotated;
-    
+
     var r = this.rect_,key = [this.url_,r.width,r.height,r.left,r.top,this.coffset_.x,this.coffset_.y].join('_');
     if(goog.isDef(this.dataCache_[key])){
         this.data_ = this.dataCache_[key];
@@ -50,7 +51,7 @@ lime.fill.Frame = function(img, rect, opt_offset, opt_size, opt_rotated) {
         this.data_.initializer = this;
         this.data_.classname = this.getNextCssClass_();
         this.dataCache_[key] = this.data_;
-        
+
         if(this.USE_CSS_CANVAS){
             this.ctx = document.getCSSCanvasContext('2d', this.data_.classname, this.csize_.width, this.csize_.height);
         }
@@ -58,7 +59,7 @@ lime.fill.Frame = function(img, rect, opt_offset, opt_size, opt_rotated) {
             //todo: FF4 has support for element backgrounds. probably faster than this png url.
             this.ctx = this.makeCanvas();
         }
-    
+
         if(this.isLoaded()){
             this.makeFrameData_();
         }
@@ -89,20 +90,20 @@ lime.fill.Frame.prototype.USE_CSS_CANVAS = goog.isFunction(document.getCSSCanvas
  * @inheritDoc
  */
 lime.fill.Frame.prototype.initForSprite = function(sprite){
-    
+
     var size = sprite.getSize();
     if(size.width==0 && size.height==0){
         sprite.setSize(this.csize_.width,this.csize_.height);
     }
-    
+
     lime.fill.Image.prototype.initForSprite.call(this,sprite);
-    
+
     if(!this.isProcessed()){
         goog.events.listen(this,'processed',function(){
             sprite.setDirty(lime.Dirty.CONTENT);
         },false,this);
     }
-    
+
     //switch to canvas if no support
 };
 
@@ -127,30 +128,31 @@ lime.fill.Frame.prototype.getNextCssClass_ = function(){
  */
 lime.fill.Frame.prototype.makeFrameData_ = function(){
     this.writeToCanvas(this.ctx);
-    
-    if(!this.USE_CSS_CANVAS){
 
-    var contents = this.cvs.toDataURL("image/png"),
-        rule = '.'+this.data_.classname+'{background-image:url('+contents+') !important}';
-    if(!styleSheet){
-        goog.style.installStyles(rule);
-       styleSheet = document.styleSheets[document.styleSheets.length-1];
-    }
-    else {
-        // why doesn't addCssRule work in IE9???
-       if(goog.userAgent.IE) styleSheet.cssText+=rule;
-       else goog.cssom.addCssRule(styleSheet,rule);
-    }
-    
-    // laoding into image to avoid flickery onf firefox firat load
-    this.data_.img = goog.dom.createDom('img');
-    this.data_.img.src = contents;
+    if(!this.USE_CSS_CANVAS && lime.dom.isDOMSupported()){
+
+        var contents = this.cvs.toDataURL("image/png"),
+            rule = '.'+this.data_.classname+'{background-image:url('+contents+') !important}';
+        if (!styleSheet) {
+           goog.style.installStyles(rule);
+           styleSheet = document.styleSheets[document.styleSheets.length-1];
+        }
+        else {
+            // why doesn't addCssRule work in IE9???
+           if(goog.userAgent.IE) styleSheet.cssText+=rule;
+           else goog.cssom.addCssRule(styleSheet,rule);
+        }
+
+        // loading into image to avoid flickery on firefox first load
+        this.data_.img = goog.dom.createDom('img');
+        this.data_.img.src = contents;
 
     }
-    
+
     this.data_.processed = true;
     this.dispatchEvent(new goog.events.Event('processed'));
-    
+    this.getImageElement().complete = true;
+
 };
 
 })();
@@ -204,15 +206,14 @@ lime.fill.Frame.prototype.writeToCanvas = function(ctx){
         ox = this.coffset_.x;
         oy = this.coffset_.y;
     }
-    
     ctx.drawImage(this.image_,l,t,w,h,ox,oy,w,h);
 };
 
 /** @inheritDoc */
 lime.fill.Frame.prototype.setDOMStyle = function(domEl,shape) {
     if(this.USE_CSS_CANVAS){
-        domEl.style['background'] = '-webkit-canvas('+this.data_.classname+')';    
-    }    
+        domEl.style['background'] = '-webkit-canvas('+this.data_.classname+')';
+    }
     else if(this.data_.classname!=shape.cvs_background_class_){
         goog.dom.classes.add(domEl,this.data_.classname);
         domEl.style['background'] = '';
@@ -220,7 +221,7 @@ lime.fill.Frame.prototype.setDOMStyle = function(domEl,shape) {
         goog.dom.classes.remove(domEl,shape.cvs_background_class_);
         shape.cvs_background_class_ = this.data_.classname;
     }
-    
+
     this.setDOMBackgroundProp_(domEl,shape);
 };
 
