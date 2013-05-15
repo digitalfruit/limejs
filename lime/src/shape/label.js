c2d28a05516bd1b16f759c29a44a0c228fa382c3
Tõnis Tiigi <tonistiigi@gmail.com>
2013-05-15 11:12:24 +0300

    Include font style for text measurement #81

--- lime/src/shape/label.js
+++ lime/src/shape/label.js
@@ -75,7 +75,8 @@ lime.Label.prototype.measureText = function() {
     if (this.getMultiline()) {
         lh *= goog.string.trim(this.text_).split('\n').length
     }
-    mContext.font = this.getFontSize() + 'px ' + this.getFontFamily();
+    mContext.font = this.getStyle() + ' ' + this.getFontWeight() + ' ' + this.getFontSize() + 'px ' + this.getFontFamily();
+
     var metrics = mContext.measureText(this.text_);
     var w = goog.userAgent.WEBKIT ? metrics.width : metrics.width + 1;
 
