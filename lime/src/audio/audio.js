13706b3ccf0d71ab3bb5622c19f4aef93173fbb7
kyawtun <okisan.2008@gmail.com>
2013-08-25 08:27:11 +0800

    ability to resume looped autio.

--- lime/src/audio/audio.js
+++ lime/src/audio/audio.js
@@ -22,56 +22,21 @@ lime.audio.Audio = function(filePath, exts, opt_volume, opt_loop) {
    * @type {number}
    */
   this.volume = goog.isDef(opt_volume) ? opt_volume : 1;
-
   /**
    * Internal audio element
    * @type {HTMLAudioElement}
    * @protected
    */
-  this.audio = /** @type {HTMLAudioElement} */ (document.createElement(
-      'audio'));
-  var codecs = { // Chart from jPlayer
-    'ogg': 'audio/ogg; codecs="vorbis"', //OGG
-    'wav': 'audio/wav; codecs="1"', // PCM
-    'webma': 'audio/webm; codecs="vorbis"', // WEBM
-    'mp3': 'audio/mpeg; codecs="mp3"', //MP3
-    'm4a': 'audio/mp4; codecs="mp4a.40.2"'// AAC / MP4
-  };
-  var ext;
-  for (var i = 0; i < exts.length; ++i) {
-    var codec = codecs[exts[i]];
-    if (goog.DEBUG && !codec) {
-      throw new Error('Invalid extension "' + exts[i] + '"');
-    }
-    if (this.audio.canPlayType(codec)) {
-      ext = exts[i];
-      break;
-    }
-  }
-  if (ext) {
-    this.audio.preload = 'auto';
-    if (opt_loop) {
-      this.audio.loop = true;
-      // this.audio.autoplay = true;
-    }
-    this.audio.src = filePath + '.' + ext;
-    this.audio.load();
-    goog.events.listen(this.audio, 'ended', function(e) {
-      this.audio.loaded = true; // somehow it changed to undefined on next play.
-      this.dispatch_();
-    }, false, this);
-  } else {
-    this.logger.warning('Audio codecs ' + JSON.stringify(exts) +
-        ' not supported');
-    this.audio = null;
-  }
-
+  this.audio = null;
   /**
    * @type {Array.<Function, scope>}
    * @private
    */
   this.listeners_ = [];
-
+  this.loop = !!opt_loop;
+  this.exts = exts;
+  this.filePath = filePath;
+  this.createAudio();
 };
 goog.inherits(lime.audio.Audio, goog.Disposable);
 
@@ -82,6 +47,51 @@ goog.inherits(lime.audio.Audio, goog.Disposable);
  */
 lime.audio.Audio.prototype.logger =
     goog.debug.Logger.getLogger('lime.audio.Audio');
+
+
+/**
+ * @protected
+ */
+lime.audio.Audio.prototype.createAudio = function() {
+
+  this.audio = /** @type {HTMLAudioElement} */ (document.createElement(
+      'audio'));
+  var codecs = { // Chart from jPlayer
+    'ogg': 'audio/ogg; codecs="vorbis"', //OGG
+    'wav': 'audio/wav; codecs="1"', // PCM
+    'webma': 'audio/webm; codecs="vorbis"', // WEBM
+    'mp3': 'audio/mpeg; codecs="mp3"', //MP3
+    'm4a': 'audio/mp4; codecs="mp4a.40.2"'// AAC / MP4
+  };
+  var ext;
+  for (var i = 0; i < this.exts.length; ++i) {
+    var codec = codecs[this.exts[i]];
+    if (goog.DEBUG && !codec) {
+      throw new Error('Invalid extension "' + this.exts[i] + '"');
+    }
+    if (this.audio.canPlayType(codec)) {
+      ext = this.exts[i];
+      break;
+    }
+  }
+  if (ext) {
+    this.audio.preload = 'auto';
+    if (this.loop) {
+      this.audio.loop = true;
+      // this.audio.autoplay = true;
+    }
+    this.audio.src = this.filePath + '.' + ext;
+    this.audio.load();
+    goog.events.listen(this.audio, 'ended', function(e) {
+      this.audio.loaded = true; // somehow it changed to undefined on next play.
+      this.dispatch_();
+    }, false, this);
+  } else {
+    this.logger.warning('Audio codecs ' + JSON.stringify(this.exts) +
+        ' not supported');
+    this.audio = null;
+  }
+};
 
 
 /**
@@ -140,6 +150,10 @@ lime.audio.Audio.prototype.play = function(opt_volume, opt_cb, opt_scope) {
       opt_cb.call(opt_scope);
     }
   } else {
+    if (this.audio.loop === true) {
+      // loop audio cannot play again, must create a new Element.
+      this.createAudio();
+    }
     this.audio.volume = goog.isDef(opt_volume) ? opt_volume : this.volume;
     if (opt_cb) {
       this.listeners_.push([opt_cb, opt_scope]);
@@ -170,6 +184,9 @@ lime.audio.Audio.prototype.stop = function() {
  * @inheritDoc
  */
 lime.audio.Audio.prototype.disposeInternal = function() {
+  if (this.audio) {
+    this.audio.pause();
+  }
   this.dispatch_();
   this.audio = null;
 };
