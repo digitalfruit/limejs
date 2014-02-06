76a650c6b887007d83036182557b7d22d38112cb
Aaren Cordova <aaren.cordova@gmail.com>
2014-02-05 16:34:51 -0800

    Updated space and tab mixes

--- lime/templates/default/__name__.js
+++ lime/templates/default/__name__.js
@@ -16,54 +16,54 @@ goog.require('lime.animation.MoveTo');
 
 // entrypoint
 {name}.start = function(){ 
-    
+	
 	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
-	    
-	    target = new lime.Layer().setPosition(512,384), 
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
-        
-    
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
-    
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
-    
+		scene = new lime.Scene(),
+		
+		target = new lime.Layer().setPosition(512,384), 
+		circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
+		lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
+		title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
+			.setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+		
+	
+	//add circle and label to target object
+	target.appendChild(circle);
+	target.appendChild(lbl);
+	
+	//add target and title to the scene
+	scene.appendChild(target);
+	scene.appendChild(title);
+	
 	director.makeMobileWebAppCapable();
-    
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
-        
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
-        
-        title.runAction(new lime.animation.FadeTo(1));
-        
-        //let target follow the mouse/finger
-        e.startDrag();
-        
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-            
-            title.runAction(new lime.animation.FadeTo(0));
-        });
-        
-        
-    });
+	
+	//add some interaction
+	goog.events.listen(target,['mousedown','touchstart'],function(e){
+		
+		//animate
+		target.runAction(new lime.animation.Spawn(
+			new lime.animation.FadeTo(.5).setDuration(.2),
+			new lime.animation.ScaleTo(1.5).setDuration(.8)
+		));
+		
+		title.runAction(new lime.animation.FadeTo(1));
+		
+		//let target follow the mouse/finger
+		e.startDrag();
+		
+		//listen for end event
+		e.swallow(['mouseup','touchend'],function(){
+			target.runAction(new lime.animation.Spawn(
+				new lime.animation.FadeTo(1),
+				new lime.animation.ScaleTo(1),
+				new lime.animation.MoveTo(512,384)
+			));
+			
+			title.runAction(new lime.animation.FadeTo(0));
+		});
+		
+		
+	});
 	
 	// set current scene active
 	director.replaceScene(scene);
