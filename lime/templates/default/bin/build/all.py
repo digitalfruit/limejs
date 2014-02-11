9ee71eb5182ce02eccf0e6b5b77d6494c1255158
Tonis Tiigi <tonistiigi@gmail.com>
2014-02-11 19:19:16 +0200

    Use relative path for all builder. Fix command.
    Issue: #124

--- lime/templates/default/bin/build/all.py
+++ lime/templates/default/bin/build/all.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
 import subprocess
+import os
+
+bindir = os.path.dirname(os.path.realpath(__file__))
 
 def main():
-	call = ''
-	call += 'python list.py '
-	call += 'python script.py '
-	call += 'python whitespace.py '
-	call += 'python simple.py '
-	call += 'python advanced.py '
-
+	call = 'python ' + os.path.join(bindir, 'list.py')
 	subprocess.call(call,shell=True)
+	call = 'python ' + os.path.join(bindir, 'script.py')
+	subprocess.call(call,shell=True)
+	call = 'python ' + os.path.join(bindir, 'whitespace.py')
+	subprocess.call(call,shell=True)
+	call = 'python ' + os.path.join(bindir, 'simple.py')
+	subprocess.call(call,shell=True)
+	call = 'python ' + os.path.join(bindir, 'advanced.py')
+	subprocess.call(call,shell=True)
+
 if __name__ == '__main__':
 	main()
